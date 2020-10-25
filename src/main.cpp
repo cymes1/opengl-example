f5888c4d2b2bc85ab968ccb27f89aecd7b8c03b1
Cymes1 <mateusz.winiarczyk@outlook.com>
2020-10-25 19:43:51 +0100

    Added index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -123,20 +123,32 @@ int main()
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	float positions[6] = {
+	float positions[] = {
 		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f,
+		 0.5f,  0.5f,
+        -0.5f,  0.5f
+	};
+
+	unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
 	};
 
 	// create vertex buffer
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, 0);
+
+    // create index buffer
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShader("res/shader/basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -148,7 +160,7 @@ int main()
 		// render here
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		// swap front and back buffers
 		glfwSwapBuffers(window);
