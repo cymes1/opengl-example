5df94fc8aa195e284122d0b41e13a2fcffe62f66
cymes1 <mateusz.winiarczyk@outlook.com>
2022-01-07 05:42:52 +0100

    Transformed 2d texture example to pure OpenGL

--- src/states/states/texture-2d-state.cpp
+++ src/states/states/texture-2d-state.cpp
@@ -11,7 +11,10 @@ namespace OpenGlExample::States
             proj(glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f)),
             view(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0))),
             translationA(200, 200, 0),
-            translationB(400, 200, 0)
+            translationB(400, 200, 0),
+			scaleA(1, 1, 1),
+			scaleB(1, 1, 1),
+			indicesCount(6)
     {
         float positions[] = {
             -50.0f, -50.0f, 0.0f, 0.0f,
@@ -25,20 +28,32 @@ namespace OpenGlExample::States
             2, 3, 0
         };
 
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 
-        vertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
-        VertexBufferLayout layout;
-        layout.push<float>(2);
-        layout.push<float>(2);
-        vertexArray = std::make_unique<VertexArray>();
-        vertexArray->addBuffer(*vertexBuffer, layout);
-        indexBuffer = std::make_unique<IndexBuffer>(indices, 6);
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, 4 * 4 * sizeof(float), positions, GL_STATIC_DRAW);
 
-        texture = std::make_unique<Texture>("res/texture/test-texture.png");
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
 
+
+		GLsizei stride = 4 * 4;
+		GLuint offset = 0;
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, false, stride, reinterpret_cast<const void*>(offset));
+		offset += 2 * 4;
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, false, stride, reinterpret_cast<const void*>(offset));
+
+
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesCount * sizeof(GLuint), indices, GL_STATIC_DRAW);
+
+		texture = std::make_unique<Texture>("res/texture/test-texture.png");
         shader = std::make_unique<Shader>("res/shader/texture-2d/basic-texture.shader");
         shader->bind();
         shader->setUniform1i("u_Texture", 0);
@@ -46,31 +61,39 @@ namespace OpenGlExample::States
 
     Texture2DState::~Texture2DState()
     {
-        shader->unbind();
-    }
+		shader->unbind();
+		glDeleteBuffers(1, &ibo);
+		glDeleteVertexArrays(1, &vao);
+		glDeleteBuffers(1, &vbo);
+	}
 
     void Texture2DState::render()
     {
-        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 0.0f));
-        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        Renderer renderer;
         texture->bind();
 
         {
             glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
             glm::mat4 mvp = proj * view * model;
+			mvp = glm::scale(mvp, scaleA);
             shader->bind();
             shader->setUniformMat4f("u_MVP", mvp);
-            renderer.draw(*vertexArray, *indexBuffer, *shader);
+			shader->bind();
+			glBindVertexArray(vao);
+			glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, nullptr);
         }
         {
             glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
             glm::mat4 mvp = proj * view * model;
+			mvp = glm::scale(mvp, scaleB);
             shader->bind();
             shader->setUniformMat4f("u_MVP", mvp);
-            renderer.draw(*vertexArray, *indexBuffer, *shader);
-        }
+			shader->bind();
+			glBindVertexArray(vao);
+			glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, nullptr);
+		}
     }
 
     void Texture2DState::renderImGui()
@@ -82,5 +105,7 @@ namespace OpenGlExample::States
         }
         ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
         ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-    }
+		ImGui::SliderFloat3("Scale A", &scaleA.x, 0.1f, 3.0f);
+		ImGui::SliderFloat3("Scale B", &scaleB.x, 0.1f, 3.0f);
+	}
 }
--- src/states/states/texture-2d-state.h
+++ src/states/states/texture-2d-state.h
@@ -14,15 +14,18 @@ namespace OpenGlExample::States
     class Texture2DState : public State
     {
     private:
-        std::unique_ptr<VertexBuffer> vertexBuffer;
-        std::unique_ptr<VertexArray> vertexArray;
-        std::unique_ptr<IndexBuffer> indexBuffer;
         std::unique_ptr<Shader> shader;
         std::unique_ptr<Texture> texture;
         glm::mat4 proj;
         glm::mat4 view;
         glm::vec3 translationA;
         glm::vec3 translationB;
+		glm::vec3 scaleA;
+		glm::vec3 scaleB;
+		GLuint vbo;
+		GLuint vao;
+		GLuint ibo;
+		GLsizei indicesCount;
     public:
         explicit Texture2DState(Root& root);
         ~Texture2DState() override;
